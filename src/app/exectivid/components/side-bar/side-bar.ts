30c1c34a0ed93433bc3900ee338428559daf0647
Diego Ramirez <diegorf339@gmail.com>
2026-06-24 12:42:20 -0500

    Agregando una barra superior que se ve solo en móviles

--- src/app/exectivid/components/side-bar/side-bar.ts
+++ src/app/exectivid/components/side-bar/side-bar.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'side-bar',
   imports: [],
   templateUrl: './side-bar.html',
 })
-export class SideBar {}
+export class SideBar {
+
+  //* Funcionalidad solo en teléfonos:
+
+  //* Esto es solo un signal con estado inicial en false.
+  //? En el código esto indicará que el side-bar siempre estará cerrado inicialmente.
+  isSidebarOpen = signal( false );
+
+  //* Un método que cambia el estado del signal a su opuesto.
+  //? En el código esto es lo que da la funcionalidad de abrir o cerrar el side-bar.
+  toggleSidebar() {
+    this.isSidebarOpen.update(estado => !estado);
+  };
+
+};
