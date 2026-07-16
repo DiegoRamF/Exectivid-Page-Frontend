9314a2687d22db18d0c0635a8dfd9ef00a9f92e5
Diego Ramirez <diegorf339@gmail.com>
2026-07-16 11:16:35 -0500

    Agregando la funcionalidad de song-list

--- src/app/exectivid/pages/song-list/song-list.ts
+++ src/app/exectivid/pages/song-list/song-list.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map, of } from 'rxjs';
+
+import { AlbumesService } from '../../services/albumes.service';
+import { SongsService } from '../../services/songs.service';
 
 @Component({
   selector: 'song-list',
   imports: [],
   templateUrl: './song-list.html',
 })
-export default class SongList {}
+export default class SongList {
+
+  private albumesService = inject( AlbumesService );
+  private songsService = inject( SongsService );
+  private route = inject( ActivatedRoute )
+
+  albumSlug = toSignal<string>(
+    this.route.params
+    .pipe(
+      map( params => params[ 'albumSlug' ] )
+    ),
+  );
+
+  albumId = computed<number | undefined>( () => {
+    const album = this.albumResource.value();
+    if( !album ) return undefined;
+    return album.id;
+  });
+
+  albumResource = rxResource({
+    params: () => ({ slug: this.albumSlug() }),
+    stream: ({ params }) => this.albumesService.getAlbumBySlug( params.slug )
+  });
+
+  songResource = rxResource({
+    params: () => ({ id: this.albumId() }),
+    stream: ({ params }) => {
+      if( params.id === undefined ) return of( undefined );
+      return this.songsService.getSongs( params.id );
+    },
+  });
+
+  albumInfo = computed( () => this.albumResource.value() );
+  songInfo = computed( () => this.songResource.value() );
+
+};
